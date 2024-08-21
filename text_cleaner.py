de61d1bd269cede0c901f833912bf2d5943c0ad9
Francesco Nicotra <84009233+FrancescoNicotra@users.noreply.github.com>
2024-08-21 18:15:44 +0200

    fix

--- text_cleaner.py
+++ text_cleaner.py
@@ -1,12 +1,9 @@
 import pandas as pd
 import numpy as np
-import csv
-#for text pre-processing
-import re, string
+import re
 import nltk
 from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
-from nltk.tokenize import word_tokenize
 from nltk.stem import SnowballStemmer
 from nltk.corpus import wordnet
 from nltk.stem import WordNetLemmatizer
@@ -14,57 +11,62 @@ from nltk.stem import WordNetLemmatizer
 WL = WordNetLemmatizer()
 
 def read_csv(path: str) -> pd.DataFrame:
-	nltk.download('punkt')
-	nltk.download('averaged_perceptron_tagger')
-	nltk.download('wordnet')
-	nltk.download('vader_lexicon')
-	nltk.download('stopwords')
+    nltk.download('punkt')
+    nltk.download('averaged_perceptron_tagger')
+    nltk.download('wordnet')
+    nltk.download('vader_lexicon')
+    nltk.download('stopwords')
 
-	# Read the CSV file
-	return pd.read_csv(path)
-
-text = read_csv('./transcriptions/Chris_Evans_OPENS_UP_About_His_‘Deadpool_&_Wolverine’_Cameo_(Exclusive)__E!_News.csv')
+    # Read the CSV file
+    return pd.read_csv(path)
 
 def clean_text(text: pd.DataFrame) -> pd.DataFrame:
-	#lambda function to transform the text to lowercase
-	text['testo'] = text['testo'].apply(lambda x: str.lower(x) if pd.isna(x) != True else x)
+    # Create 'clean_text' column by copying 'testo' column
+    text['clean_text'] = text['testo'].apply(lambda x: str.lower(x) if pd.notna(x) else x)
 
-	#lambda function to remove the punctuation
-	text['clean_text'].apply(lambda x: re.sub(r'[^\w\s]', ' ', x) if pd.notna(x) else x)
-	text['clean_text'] = text['clean_text'].apply(lambda x: re.sub(r'[\n\t]', ' ', x) if pd.notna(x) else x)
+    # Remove punctuation and replace newlines/tabs
+    text['clean_text'] = text['clean_text'].apply(lambda x: re.sub(r'[^\w\s]', ' ', x) if pd.notna(x) else x)
+    text['clean_text'] = text['clean_text'].apply(lambda x: re.sub(r'[\n\t]', ' ', x) if pd.notna(x) else x)
+    return text
 
 def get_wordnet_pos(tag):
-	if tag.startswith('J'):
-		return wordnet.ADJ
-	elif tag.startswith('V'):
-		return wordnet.VERB
-	elif tag.startswith('N'):
-		return wordnet.NOUN
-	elif tag.startswith('R'):
-		return wordnet.ADV
-	else:
-		return wordnet.NOUN
+    if tag.startswith('J'):
+        return wordnet.ADJ
+    elif tag.startswith('V'):
+        return wordnet.VERB
+    elif tag.startswith('N'):
+        return wordnet.NOUN
+    elif tag.startswith('R'):
+        return wordnet.ADV
+    else:
+        return wordnet.NOUN
 
 def lemmatizer(string):
-	word_pos_tags = nltk.pos_tag(word_tokenize(string))
-	a = [WL.lemmatize(tag[0], get_wordnet_pos(tag[1])) for idx, tag in enumerate(word_pos_tags)]
-	return " ".join(a)
+    word_pos_tags = nltk.pos_tag(word_tokenize(string))
+    a = [WL.lemmatize(tag[0], get_wordnet_pos(tag[1])) for idx, tag in enumerate(word_pos_tags)]
+    return " ".join(a)
 
 def lemmatize_text(text: pd.DataFrame) -> pd.DataFrame:
-	return text['clean_text'].apply(lambda x: lemmatizer(x) if pd.isna(x) != True else x)
+    return text['clean_text'].apply(lambda x: lemmatizer(x) if pd.notna(x) else x)
 
 def stopword(string):
-	a = [i for i in string.split() if i not in stopwords.words('english')]
-	return ' '.join(a)
+    a = [i for i in string.split() if i not in stopwords.words('english')]
+    return ' '.join(a)
 
 def remove_empty_rows(text: pd.DataFrame) -> pd.DataFrame:
-	text['clean_text'].replace('', np.nan,inplace=True)
-	return text.dropna(subset=['clean_text'], inplace=True)
+    text['clean_text'].replace('', np.nan, inplace=True)
+    text.dropna(subset=['clean_text'], inplace=True)
+    return text
 
 if __name__ == "__main__":
-	clean_text(text)
-	text['clean_text'] = lemmatize_text(text)
-	text['clean_text'] = text['clean_text'].apply(lambda x: stopword(x) if pd.isna(x) != True else x)
-	remove_empty_rows(text)
-	text.to_csv('./transcriptions/Chris_Evans_OPENS_UP_About_His_‘Deadpool_&_Wolverine’_Cameo_(Exclusive)__E!_News_clean.csv', index=False)
-	print(text)
+    path = './transcriptions/Chris_Evans_OPENS_UP_About_His_‘Deadpool_&_Wolverine’_Cameo_(Exclusive)__E!_News.csv'
+    text = read_csv(path)
+    text = clean_text(text)
+    text['clean_text'] = lemmatize_text(text)
+    text['clean_text'] = text['clean_text'].apply(lambda x: stopword(x) if pd.notna(x) else x)
+    text = remove_empty_rows(text)
+
+    # Sovrascrivi il file CSV originale
+    text.to_csv(path, index=False, encoding='utf-8')
+
+    print(text)
